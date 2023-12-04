feed0ace4428cd3e43964f0a3b56293d3233e7e6
Hiago nunes pimentel <derundaclash665@gmail.com>
2023-12-04 12:30:53 -0300

    Lógica de xeque!

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,29 +4,31 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
-public abstract class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece {
 
     private Color color;
 
-
-
-
-    public ChessPiece(Board board , Color color) {
+    public ChessPiece(Board board, Color color) {
         super(board);
-        this.color= color;
+        this.color = color;
     }
-    
-    //Getter
+
+    // Getter
     public Color getColor() {
         return color;
     }
-    //Setter
+
+    public ChessPosition getChessPosition(){
+        return ChessPosition.fromPosition(position);
+    }
+
+    // Setter
     public void setColor(Color color) {
         this.color = color;
     }
 
-    protected boolean isThereOppentPiece(Position position){
-        ChessPiece p = (ChessPiece)getBoard().piece(position);
+    protected boolean isThereOppentPiece(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
 
     }
