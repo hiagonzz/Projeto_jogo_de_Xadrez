68922367d3de388925950933025002451cd4de2c
Hiago nunes pimentel <derundaclash665@gmail.com>
2023-12-05 00:26:04 -0300

    Contagem de Movimentos das peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -84,7 +84,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturePiece = board.removePiece(target);
         board.placePiece(p, target);
         if (capturePiece != null) {
@@ -96,7 +97,8 @@ public class ChessMatch {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
 
         if (capturedPiece != null) {
@@ -132,7 +134,7 @@ public class ChessMatch {
     }
 
     private Color opponent(Color color) {
-        return (color == Color.WHITE) ? Color.BLACK : Color.WHITE;
+        return (color == Color.WHITE) ? Color.BLACK : Color.WHITE;                  
 
     }
 
